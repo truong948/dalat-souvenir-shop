a845e47039cf886fd0896c553a5dd4423cf3db11
Nguyen Van Truong <truong77766t@gmail.com>
2026-06-10 22:33:45 +0700

    Fix connection string name

--- EcommerceMVC/Program.cs
+++ EcommerceMVC/Program.cs
@@ -7,8 +7,9 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
-builder.Services.AddDbContext<Hshop2023Context>(options => {
-	options.UseSqlServer(builder.Configuration.GetConnectionString("HShop"));
+builder.Services.AddDbContext<Hshop2023Context>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 
 builder.Services.AddDistributedMemoryCache();
